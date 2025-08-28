c56f31d929688ab3358626437cab162e3871b95f
Manasa Mamidi <nagaiahmanumamidi@gmail.com>
2025-08-28 23:18:08 +0530

    Added Palindrome program

--- Palindrome.java
+++ Palindrome.java
@@ -1,22 +1,20 @@
 import java.util.Scanner;
-public class Palindrome {
+public class Palindromenumber{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out,println("Enter a number:");
         int n=sc.nextInt();
         int rev=0;
         int original=n;
-
-        while (n != 0) {
-          rev = rev*10+n%10;
+        while(n != 0){
+            rev = rev*10+n%10;
             n=n/10;
         }
-
-        if (original == rev) {
+        if(original==rev){
             System.out.println(original + " is a palindrome");
-        } else {
+        }else{
             System.out.println(original + " is not a palindrome");
         }
         sc.close();
-    }
+    }
 }
