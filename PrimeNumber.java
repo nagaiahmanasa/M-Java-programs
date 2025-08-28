6039aac927e01dc486936e40fc9a7f3a3b973079
Manasa Mamidi <nagaiahmanumamidi@gmail.com>
2025-08-28 23:44:09 +0530

    Added PrimeNumber program

---
+++ PrimeNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Primenumber{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    int n=sc.nextInt();
+    int i;
+    int boolean isPrime=true;
+    if(n<=1){
+      isPrime=false;
+    }else{
+      for(i=2;i<=n/2;i++){
+        if(n%i==0){
+          isprime=false;
+          break;
+        }
+      }
+    }
+    if(isPrime){
+      System.out.println(n + " is a prime number");
+    }else{
+      System.out.println(n + " is not a prime number");
+    }
+  }
+}
+  
