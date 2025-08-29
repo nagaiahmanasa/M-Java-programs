19bcb859353a37c479fe8d164b153458cb887b96
Manasa Mamidi <nagaiahmanumamidi@gmail.com>
2025-08-29 08:22:07 +0530

    Update EvenOddCheck.java

--- EvenOddCheck.java
+++ EvenOddCheck.java
@@ -1,5 +1,5 @@
 import java.util.scanner;
-public class Evenoddcheck{
+public class EvenOddCheck{
   public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
