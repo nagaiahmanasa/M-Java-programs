53cee640c6b1addb400070d8ee8d80ace41cdaee
Manasa Mamidi <nagaiahmanumamidi@gmail.com>
2025-08-29 08:16:32 +0530

    Update and rename EvenOdd.java to EvenOddCheck.java

--- EvenOddCheck.java
+++ EvenOddCheck.java
@@ -1,5 +1,5 @@
 import java.util.scanner;
-public class Evenodd{
+public class Evenoddcheck{
   public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
