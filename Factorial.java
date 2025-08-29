18daeb896af2a15bab7259c345e3ad26c93bfef3
Manasa Mamidi <nagaiahmanumamidi@gmail.com>
2025-08-29 09:29:37 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter a number");
+        System.out.println("Enter a number: ");
         int n=sc.nextInt();
         int i,fact=1;
         for(i=1;i<=n;i++){
             fact=fact*i;
         }
-        System.out.println("factorial of " + n + " = " + fact);
+        System.out.println("Factorial of " + n + " = " + fact);
         sc.close();
     }
 }
