b2330b778bd130280c55e47db0e434c29bcd4450
Manasa Mamidi <nagaiahmanumamidi@gmail.com>
2025-08-29 09:28:51 +0530

    Update SumNatural.java

--- SumNatural.java
+++ SumNatural.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class SumNatural {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter number of terms");
+        System.out.println("enter n terms");
         int n=sc.nextInt();
         int i,sum=0;
         for(i=1;i<=n;i++){
