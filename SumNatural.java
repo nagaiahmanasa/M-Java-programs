a6e11e10329cb87f0841ecd032b5887bcaf7b706
Manasa Mamidi <nagaiahmanumamidi@gmail.com>
2025-08-29 08:47:41 +0530

    Added SumNatural program

---
+++ SumNatural.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class SumNatural {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number of terms");
+        int n=sc.nextInt();
+        int i,sum=0;
+        for(i=1;i<=n;i++){
+            sum+=i;
+        }
+        System.out.println("Sum of first " + n + " natural number= " + sum);
+        sc.close();
+    }
+}
